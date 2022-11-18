0fbfa862493e78ac7f72ef18c2f33af1325147e8
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-11-18 17:32:17 -0300

    reducing dependencies with interface for eventspanel service

---
+++ src/AMIHubClientOptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Sufficit.Telephony.EventsPanel
+{
+    public class AMIHubClientOptions
+    {
+        public const string SECTIONNAME = "Sufficit:Telephony:AMIHubClient";
+
+        [JsonPropertyName("endpoint")]
+        public Uri EndPoint { get; set; } = default!;
+
+        public Exception? Validate()
+        {
+            if(EndPoint != null)
+            {
+                return null;
+            } 
+            else { return new Exception("null or empty endpoint"); }
+        }
+
+        public AMIHubClientOptions ValidateAndThrow()
+        {
+            var ex = Validate();
+            if(ex != null) throw ex;
+
+            return this;
+        }
+    }
+}
---
+++ src/IEventsPanelService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Sufficit.Telephony.EventsPanel
+{
+    public interface IEventsPanelService
+    {
+        void Configure(AMIHubClientOptions options);
+
+        bool IsConfigured { get; }
+
+        Task StartAsync(CancellationToken cancellationToken);
+
+        Task StopAsync(CancellationToken cancellationToken);
+    }
+}
